85384e35da24e0242695a6b7a4543f62307af3f8
前田竜二 <maedaryuji@maedaryuujinoMacBook-Pro.local>
2023-03-09 22:44:05 +0900

    1st

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from array import array
 import os
 from PIL import Image
 import sys
-import time
 
+import time
 import json
 
 with open('secret.json') as f:
@@ -40,3 +40,34 @@ def detect_objects(filepath):
 import streamlit as st
 from PIL import ImageDraw
 from PIL import ImageFont
+
+st.title('物体検出アプリ')
+
+uploaded_file = st.file_uploader('Choose an image...', type=['jpg','png'])
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    img_path = f'img/{uploaded_file.name}'
+    img.save(img_path)
+    objects = detect_objects(img_path)
+
+    # 描画
+    draw = ImageDraw.Draw(img)
+    for object in objects:
+        x = object.rectangle.x
+        y = object.rectangle.y
+        w = object.rectangle.w
+        h = object.rectangle.h
+        caption = object.object_property
+
+        font = ImageFont.truetype(font='./Helvetica 400.ttf', size=50)
+        text_w, text_h = draw.textsize(caption, font=font)
+
+        draw.rectangle([(x, y), (x+w, y+h)], fill=None, outline='green', width=5)
+        draw.rectangle([(x, y), (x+text_w, y+text_h)], fill='green')
+        draw.text((x, y), caption, fill='white', font=font)
+
+    st.image(img)
+    tags_name = get_tags(img_path)
+    tags_name = ', '.join(tags_name)
+    st.markdown('**認識されたコンテンツタグ**')
+    st.markdown(f'> {tags_name}')    
