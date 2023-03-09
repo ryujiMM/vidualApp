35b47bab16f110341503c7dbd4669e21298b9522
前田竜二 <maedaryuji@maedaryuujinoMacBook-Pro.local>
2023-03-09 10:23:30 +0900

    変更内容の概要を記述するメッセージ

---
+++ app.py
@@ -0,0 +1,42 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+import json
+
+with open('secret.json') as f:
+    secret = json.load(f)
+
+KEY = secret['KEY']
+ENDPOINT = secret['ENDPOINT']
+
+computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+def get_tags(filepath):
+    local_image = open(filepath, "rb")
+
+    tags_result = computervision_client.tag_image_in_stream(local_image)
+    tags = tags_result.tags
+    tags_name = []
+    for tag in tags:
+        tags_name.append(tag.name)
+        
+    return tags_name
+
+def detect_objects(filepath):
+    local_image = open(filepath, "rb")
+
+    detect_objects_results = computervision_client.detect_objects_in_stream(local_image)
+    objects = detect_objects_results.objects
+    return objects
+
+import streamlit as st
+from PIL import ImageDraw
+from PIL import ImageFont
